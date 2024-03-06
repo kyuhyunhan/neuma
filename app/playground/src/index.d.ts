ceb6424d4780def95854411cdcec78269a1c5f0e
kyuhyun han <kyuhyunhaan@gmail.com>
2024-03-06 10:34:29 +0100

    solve type declarion issue

---
+++ app/playground/src/index.d.ts
@@ -0,0 +1,2 @@
+// 이거 왜 필요한걸까?  
+declare module "neuma";
--- lib/neuma/vite.config.ts
+++ lib/neuma/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [
     dts({
       insertTypesEntry: true,
+      outDir: "dist/types",
     }),
   ],
   build: {
